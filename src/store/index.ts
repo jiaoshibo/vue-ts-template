bf9ec3d187e3d9d94d94b096d5765d3076e5a47c
jiaoshibo <jiaoshibo@cx.sdt.com>
2021-04-29 11:31:22 +0800

    store传参取消参数类型

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,30 +3,29 @@ const defaultState = {
   count:0,
   avatar_url:''
 }
-interface objType{count?:number,url?:string}
 export default createStore({
   state(){
     return defaultState
   },
   mutations: {
-    increment(state: typeof defaultState,obj:objType){
-      state.count = obj.count!
+    increment(state: typeof defaultState,obj){
+      state.count = obj.count
     },
     reset(state: typeof defaultState){
       state.count = 0;
     },
-    set_avatar_url(state:typeof defaultState,obj:objType){
-      state.avatar_url = obj.url!
+    set_avatar_url(state:typeof defaultState,obj){
+      state.avatar_url = obj.url
     }
   },
   actions: {
-    increment(context,obj:objType){
+    increment(context,obj){
       context.commit('increment',obj)
     },
     reset(context){
       context.commit('reset')
     },
-    set_avatar_url(context,obj:objType){
+    set_avatar_url(context,obj){
       context.commit('set_avatar_url',obj)
     }
   },
