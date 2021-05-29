cb4d135b2c5f7c7a1956d0511cd33e373f913bcb
jiaoshibo <jiaoshibo@cx.sdt.com>
2021-05-29 10:33:09 +0800

    升级elementui的版本

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -88,6 +88,9 @@ export default defineComponent({
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+.hello{
+  text-align:center;
+}
 h3 {
   margin: 40px 0 0;
 }
