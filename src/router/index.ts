707575b98a37fd783affd5dacfa8b878a27f2e4b
jiaoshibo <jiaoshibo@syngo.net>
2021-06-24 10:21:37 +0800

    添加路由元信息

--- src/router/index.ts
+++ src/router/index.ts
@@ -5,11 +5,13 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/home',
     name: 'Home',
+    meta:{title:'home'},
     component: Home
   },
   {
     path: '/about',
     name: 'About',
+    meta:{title:'about'},
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -18,6 +20,7 @@ const routes: Array<RouteRecordRaw> = [
   {
     path:'/naive',
     name:'naive',
+    meta:{title:'naive'},
     component:()=>import('../views/Naive.vue')
   }
 ]
@@ -28,6 +31,7 @@ const router = createRouter({
 })
 
 router.beforeEach((to,from,next)=>{
+  document.title=to.meta.title?<string>to.meta.title:'vue-ts-template'
   if(to.path==='/'){
     next({path:'/home'})
   }else{
--- vue.config.js
+++ vue.config.js
@@ -2,6 +2,13 @@ module.exports = {
   publicPath: '/',
   outputDir: 'dist',
   assetsDir:'public',
+  productionSourceMap:false,
+  chainWebpack:config=>{
+    config.plugin('html').tap(args=>{
+      args[0].title='vue-ts-template';
+      return args;
+    })
+  },
   devServer:{
     host:'0.0.0.0',
     port:'8080',
